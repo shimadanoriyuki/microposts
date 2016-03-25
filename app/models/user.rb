05f611c6b813e3976dc5ad29cc827b6fbb70d44b
shimadanoriyuki <admin@mail.nsproduce.com>
2016-03-25 12:20:39 +0000

    Update profile

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,9 @@ class User < ActiveRecord::Base
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
-  #validates :tel, length: {maximum:20},
-  #                numericality: { only_integer: true }
-  #validates :location, length: {maximum:120}
-  #validates :contents, length: {maximum:600}
+  VALID_TEL_REGEX = /\A[0-9-]/
+  validates :tel, length: {maximum:20},format: { with: VALID_TEL_REGEX }
+  validates :location, length: {maximum:120}
+  validates :contents, length: {maximum:600}
   has_secure_password
 end
