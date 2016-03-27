4c566b668f47ceca42323539fef28451e13b52c0
shimadanoriyuki <admin@mail.nsproduce.com>
2016-03-27 10:49:29 +0000

    Review#2 Update user profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
+  before_action :set_id, only: [:show, :edit, :update]
   
   def show
-   @user = User.find(params[:id])
   end
   
   def new
@@ -19,17 +19,22 @@ class UsersController < ApplicationController
   end
   
   def edit
-    @user = User.find(params[:id])
+    if @user != current_user
+      redirect_to @user , notice: '自分以外のプロフィールを編集することはできません'
+    end
   end
   
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:success] = "Success Update!"
       redirect_to @user
     else
       render 'edit'
     end
+  end
+  
+  def set_id
+    @user = User.find(params[:id])
   end
 
   private
